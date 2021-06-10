aaec9d0d301357e0c7163e474f4748a49599e3b2
Amanda <amandalphail@gmail.com>
2021-06-09 20:53:00 -0400

    light styling

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const BASE_URL = 'http://localhost:3000'
 
+const submit = document.querySelector('.location')
+
 async function getSearchResults(query) {
   try {
     const searchResult = await axios.get(`${BASE_URL}/api/v1/search.json?query=${query}`)
@@ -9,6 +11,8 @@ async function getSearchResults(query) {
     console.log("Error Message:", err.message)
   }
 }
+
+
 
 getSearchResults("kitten")
 //will have user input here on submit
