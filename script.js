0c2463cf18e34964514f303e279df1e3d2fb6da3
Amanda <amandalphail@gmail.com>
2021-06-09 20:16:18 -0400

    axios call working

---
+++ script.js
@@ -0,0 +1,15 @@
+const BASE_URL = 'http://localhost:3000'
+
+async function getSearchResults(query) {
+  try {
+    const searchResult = await axios.get(`${BASE_URL}/api/v1/search.json?query=${query}`)
+    
+    console.log(searchResult)
+  } catch (err) {
+    console.log("Error Message:", err.message)
+  }
+}
+
+getSearchResults("kitten")
+//will have user input here on submit
+//display on page: name, info
